f4300ae895d6fc55a06b864db059bfc239a0b72e
Nikita12321412 <105320650+Nikita12321412@users.noreply.github.com>
2022-06-27 23:50:17 +0300

    pop-up updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 const popUp = document.getElementById('pop-up')
 const popUpShow = document.querySelector('body');
 const popUpClose = document.getElementById('pop-up-close')
-const outsideBannerClose = document.querySelector('pop-up-container')
+const outsideBannerClose = document.getElementById('pop-up-container')
 
 popUpShow.addEventListener('mouseleave', function(e){
     e.preventDefault();
@@ -12,3 +12,7 @@ popUpShow.addEventListener('mouseleave', function(e){
 popUpClose.addEventListener('click', () => {
     popUp.classList.remove('active');
 });
+
+outsideBannerClose.addEventListener('click', (event) => {
+    popUp.classList.remove('active');
+})
