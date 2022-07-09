bc50216465f7be0dde9e723d02f7c936d1c885cf
Nikita12321412 <105320650+Nikita12321412@users.noreply.github.com>
2022-07-10 00:20:34 +0300

    popup upds

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ const popUpShow = document.querySelector('body');
 const popUpClose = document.getElementById('pop-up-close');
 const popUpContainer = document.getElementById('pop-up-container')
 
+let date = new Date ('10 July 2022 23:56:00')
+
 popUpShow.addEventListener('mouseleave', function(e){
     e.preventDefault();
     popUp.classList.add('active');
@@ -18,3 +20,27 @@ popUp.addEventListener('click', (event) => {
         popUp.classList.remove('active');
     }
 })
+
+
+function popUpTimer () {
+    let currentDate = new Date ();
+    let timeLeft = date - currentDate;
+    let days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(timeLeft / 1000 / 60) % 60;
+    let seconds = Math.floor(timeLeft / 1000) % 60;
+
+    if (timeLeft < 0) {
+        document.getElementById('timer-header').innerText = 'Оставьте заявку';
+        document.getElementById('pop-up__values').innerText = '';
+    } else {
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+    }
+}
+
+setInterval (popUpTimer, 1000)
+
+popUpTimer ();
